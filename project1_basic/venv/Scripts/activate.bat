f64a477915589bad138fa85058e4ffe772ae8f7f
heesootory <93choiheesoo@gmail.com>
2022-04-10 14:47:49 +0900

    autopep설치

--- project1_basic/venv/Scripts/activate.bat
+++ project1_basic/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\93cho\projectLion\project1_basic\venv
+set VIRTUAL_ENV=C:\Users\93cho\Desktop\heesootory\Django_project_like_lion\project1_basic\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
